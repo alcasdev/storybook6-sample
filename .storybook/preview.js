08ac39de3d7ea66322b6e12ab15880f02984b285
Alfonso Castañeda <al.cas89@gmail.com>
2021-07-27 18:12:50 -0500

    Include mdx examples with additional config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
-
-export const parameters = {
+const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
@@ -8,3 +7,22 @@ export const parameters = {
     },
   },
 }
+const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', right: '🇺🇸', title: 'English' },
+        { value: 'fr', right: '🇫🇷', title: 'Français' },
+        { value: 'es', right: '🇪🇸', title: 'Español' },
+        { value: 'zh', right: '🇨🇳', title: '中文' },
+        { value: 'kr', right: '🇰🇷', title: '한국어' },
+      ],
+    },
+  }
+};
+
+export { parameters, globalTypes}
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -7,6 +7,7 @@ export default {
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
+    onClick: { action: 'clicked' }
   },
 };
 
@@ -14,23 +15,27 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
+  backgroundColor: '!hex(1ea7fd)',
   primary: true,
   label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
+  backgroundColor: '!hex(00000000)',
   label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
+  backgroundColor: '!hex(00000000)',
   size: 'large',
   label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
+  backgroundColor: '!hex(00000000)',
   size: 'small',
   label: 'Button',
 };
